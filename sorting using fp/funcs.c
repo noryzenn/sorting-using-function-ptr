4ed2750dd5d2a0ab4459fc3eb04b3992d23eef62
Hesenbey Hemidov <hemidovhesenbey@gmail.com>
2025-06-27 13:06:27 +0400

    added int value variable to addElement function

--- sorting using fp/funcs.c
+++ sorting using fp/funcs.c
@@ -3,6 +3,6 @@
 #include "shellSort.h"
 
 void resizeArray(int** arr, int* capacity, int newCapacity);
-void addElement(int** arr, int* size, int* capacity); // calls resizeArray() if (size == capacity)
+void addElement(int** arr, int* size, int* capacity, int value); // calls resizeArray() if (size == capacity)
 void setSortFunction(void (**sortFunction)(int*, int), void(*chosenFunc)(int*, int));
 void sortArray(int* arr, int size, void(*sortFunction)(int*, int));
--- sorting using fp/funcs.h
+++ sorting using fp/funcs.h
@@ -2,7 +2,7 @@
 #define FUNCS_H
 
 void resizeArray(int** arr, int* capacity, int newCapacity);
-void addElement(int** arr, int* size, int* capacity); // calls resizeArray() if (size == capacity)
+void addElement(int** arr, int* size, int* capacity, int value); // calls resizeArray() if (size == capacity)
 void setSortFunction(void (**sortFunction)(int*, int), void(*chosenFunc)(int*, int));
 void sortArray(int* arr, int size, void(*sortFunction)(int*, int));
 
